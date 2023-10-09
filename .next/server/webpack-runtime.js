3e84106d66e0710b043750b16aa742689aa9d156
HRM0 <103372533+HRM0@users.noreply.github.com>
2023-10-09 19:15:53 -0400

    main section

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("438aa60611fb0d2a")
+/******/ 		__webpack_require__.h = () => ("ac5c4d65a8588eff")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/components/HeroSection.js
+++ app/components/HeroSection.js
@@ -1,9 +1,57 @@
+"use client"
 import React from "react";
+import Image from 'next/image'
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
     return (
         <section>
-            <h1 className='text-white'>Hello, I'm Rob</h1>
+            <div className='grid grid-cols-1 sm:grid-cols-12'> 
+                <div className='col-span-7 place-self-center text-center sm:text-left'>
+                    <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-green-700">
+                            Hello, I'm{" "}
+                        </span>
+                        <br></br>
+                        <TypeAnimation
+                            sequence={[
+                                // Same substring at the start will only be typed out once, initially
+                                'Rob',
+                                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                'Web Developer',
+                                1000,
+                                'Javascript Enthusiast',
+                                1000,
+                                'Musician',
+                                1000
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                        />
+                    </h1>
+                    <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    </p>
+                    <div>
+                        <button className="px-6 py-3 w-full rounded-full sm:w-fit mr-4 bg-white hover:bg-slate-200 text-black bg-gradient-to-r  from-blue-600  via-blue-400 to-green-700">Hire Me</button>
+                        <button className="px-1 py-1 w-full rounded-full sm:w-fit bg-transparent hover:bg-slate-800 text-white mt-3 bg-gradient-to-r  from-blue-600  via-blue-400 to-green-700">
+                            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download Resume</span>
+                        </button>
+                    </div>
+                </div>
+                <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+                    <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[300px] lg:h-[300px] relative">
+                        <Image 
+                            src='/images/noback.png'
+                            alt='Profile Picture'
+                            className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                            width={300}
+                            height={300}
+                        />
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
